73546e6ddbb1dd69277b026e874f40281eb36895
rebandsarah <39635012+rebandsarah@users.noreply.github.com>
2018-06-14 08:43:06 +0000

    Create score.c

---
+++ score.c
@@ -0,0 +1,32 @@
+// file2.cpp
+
+#include "myheader.h"
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+void printGame(struct Game* question)
+{
+  std::cout << "printing from a different cpp file!";
+}
+
+printf("\n");
+
+  strcpy(firstname);
+ 
+  strcpy(lastname);
+
+  for (int i = 0; i < 2; i++)
+         {
+        printf("Player Names:  %d:\n", i+1);
+        printf("\n");
+         }
+
+}
+ 
+void printGame(struct Game* question)
+{
+  printf(" First name: %s\n", student->firstname);
+  printf(" Last name: %s\n", student->lastname);
+}
+
